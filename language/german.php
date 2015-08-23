70ab3be01f9d99d4f9599a1e347740969613efdb
darkmaeg <dinodox@yahoo.com>
2015-08-23 11:06:00 -0400

    Create german.php

---
+++ language/german.php
@@ -0,0 +1,52 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2013-01-04 23:03:33 +0100 (Fri, 04 Jan 2013) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: hoofy_leon $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 12732 $
+ * 
+ * $Id: german.php 12732 2013-01-04 22:03:33Z hoofy_leon $
+ */
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+$lang = array(
+	'eq2progress_f_eq2progress_headtext'	=> 'Titel des Moduls',
+	'eq2progress_f_eq2progress_contested'	=> 'Contested Avatars',
+	'eq2progress_f_eq2progress_arena'		=> 'Arena of the Gods',
+	'eq2progress_f_eq2progress_harrows'		=> 'Harrow\'s End',
+	'eq2progress_f_eq2progress_sleepers'	=> 'Sleeper\'s Tomb',
+	'eq2progress_f_eq2progress_abhorrence'	=> 'Altar of Abhorrence',
+	'eq2progress_f_eq2progress_plane'   	=> 'Plane of War',
+	'eq2progress_f_eq2progress_dreadcutter'	=> 'The Dreadcutter',
+	'eq2progress_f_eq2progress_sirens' 		=> 'Siren\'s Grotto',
+	'eq2progress_f_eq2progress_desert' 		=> 'Desert of Flames',
+	'eq2progress_f_eq2progress_veeshan' 	=> 'Temple of Veeshan',
+	'eq2progress_f_eq2progress_accursed'	=> 'Accursed Sanctum',
+	'eq2progress_f_eq2progress_vesspyr'		=> 'Vesspyr Contested',
+	'eq2progress_f_eq2progress_kingdom'		=> 'Kingdom of Sky',
+	'eq2progress_f_eq2progress_dreadscale'  => 'Dreadscale\'s Maw',
+	'eq2progress_f_eq2progress_deathtoll'   => 'Deathtoll',
+	'eq2progress_f_eq2progress_agesend'     => 'Age\'s End',
+	'eq2progress_f_eq2progress_altar1'      => 'Zavith\'Loa',
+	'eq2progress_f_eq2progress_altar2'      => 'Castle Highhold',
+	'eq2progress_f_eq2progress_altar3'      => 'Brokenskull Bay',
+	'eq2progress_f_eq2progress_altar4'      => 'Temple of Ssraeshza',
+	'eq2progress_f_eq2progress_altar5'      => 'Cathedral of Bones',
+	'eq2progress_f_eq2progress_altar6'      => 'Altar of Malice',
+	'eq2progress_f_eq2progress_aomavatar'   => 'Precipice of Power',
+	'eq2progress_f_eq2progress_fsdistillery'=> 'Far Seas Distillery',
+	'eq2progress_f_eq2progress_freethinkers'=> 'Echoes of Faydwer',
+	'eq2progress_f_eq2progress_date'	  	=> 'Show Date Killed?<BR>*Note: Data is updated every 24 hours.<BR>Changing date setting will take effect next update.',
+);
+?>
